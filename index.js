fb1d2b06a4208c16f42f18f80cad93aef07f8724
Verdi Anggriawan <128593632+vrdgn@users.noreply.github.com>
2023-03-23 22:24:03 +0700

    Add files via upload
    Intro to Software Engineering with RevoU

---
+++ index.js
@@ -0,0 +1,8 @@
+let button = document.getElementById("button");
+
+button.addEventListener(click, () => {
+    const JenisKelamin = parseInt(document.getElementById("Jenis Kelamin").value);
+    const beratbadan = parseInt(document.getElementById("berat badan").value);
+    const usia = parseInt(document.getElementById("usia").value);
+    const tinggibadan = parseInt(document.getElementById("tinggi badan").value);
+}); 
